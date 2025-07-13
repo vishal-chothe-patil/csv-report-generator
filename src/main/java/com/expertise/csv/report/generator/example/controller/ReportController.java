8e77ce35383738b11929d006206c5392421705ab
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-07-13 16:57:16 +0530

    feat: add Customer and Loan entities, update ReportController

--- src/main/java/com/expertise/csv/report/generator/example/controller/ReportController.java
+++ src/main/java/com/expertise/csv/report/generator/example/controller/ReportController.java
@@ -1,5 +1,7 @@
 package com.expertise.csv.report.generator.example.controller;
 
+import com.expertise.csv.report.generator.example.service.CustomerServiceI;
+import com.expertise.csv.report.generator.example.service.LoanServiceI;
 import com.expertise.csv.report.generator.example.service.ReportServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     @Autowired
+    private CustomerServiceI customerServiceI;
+
+    @Autowired
+    private LoanServiceI loanServiceI;
+
+    @Autowired
     private ReportServiceI reportServiceI;
+
 }
--- src/main/java/com/expertise/csv/report/generator/example/model/Loan.java
+++ src/main/java/com/expertise/csv/report/generator/example/model/Loan.java
@@ -1,10 +1,30 @@
 package com.expertise.csv.report.generator.example.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loanType;
+    private Double amount;
+    private String status;
+    private LocalDate appliedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
 }
